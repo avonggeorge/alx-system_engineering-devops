76cf2c60097c23e903c8a872d2249e423a8dda9c
avonggeorge <georgeavong@yahoo.com>
2024-08-17 18:37:31 +0100

    Removed trailing white space

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # This file contains a Puppet manifest that fixes a bug in wp-setings.php
 
 exec { 'fix the php extension issue':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',                                                                 path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
