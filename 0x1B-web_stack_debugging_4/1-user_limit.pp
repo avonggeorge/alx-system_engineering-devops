a207cfc7cd983a884ff894c30e85dfdf78e3b69b
avonggeorge <georgeavong@yahoo.com>
2024-08-21 00:12:43 +0100

    Change configuration settings

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# Configuration that changes OS so that its possible to login with 'holberton' user and open a file without error
+
+exec { 'increase-hard-file-limit-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
